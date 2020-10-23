b127d03c676ec1bfc73cd0e3f76f06ae02206402
TimmmyD <tmd112593@gmail.com>
2020-10-23 15:45:55 -0700

    commit tree 4bdeedf4e95536d12c331fa04789186083863d4a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+    sentence = ""
+    array.each do |word|
+        if array.length == 1
+            return word
+        end
+        if array.last == word
+            sentence <<  "and " << word 
+        elsif array.length == 2
+            sentence << word << " "
+        else
+            sentence << word << ", "
+        end
+    end
+    return sentence
 
 end
